d5e0c0616fb1d7275cb7433ba3dede856627638a
LucasVGP <lucaas.vgp@gmail.com>
2024-10-25 18:18:41 -0300

    adiciona projeto

---
+++ TodoApp/ViewModel/DetailViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace TodoApp.ViewModel;
+
+[QueryProperty("Text", "Text")]
+public partial class DetailViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string text;
+
+    [RelayCommand]
+    private async Task GoBack()
+    {
+        await Shell.Current.GoToAsync("..");
+    }
+}
---
+++ TodoApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,59 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace TodoApp.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+    private IConnectivity _connectivity;
+
+    public MainViewModel(IConnectivity connectivity)
+    {
+        items = new ObservableCollection<string>();
+        text = "";
+        _connectivity = connectivity;
+    }
+
+    [ObservableProperty]
+    private string text;
+
+    [ObservableProperty]
+    private ObservableCollection<string> items;
+
+    [RelayCommand]
+    private async Task Add()
+    {
+        if (_connectivity.NetworkAccess != NetworkAccess.Internet)
+        {
+            await Shell.Current.DisplayAlert("verificação de internet", "sem internet", "Ok");
+            return;
+        }
+
+        if (string.IsNullOrEmpty(Text)) return;
+        Items.Add(Text);
+        Text = string.Empty;
+    }
+
+    [RelayCommand]
+    private void Remove(string s)
+    {
+        if (Items.Contains(s))
+        {
+            Items.Remove(s);
+        }
+    }
+
+    [RelayCommand]
+    private async Task Tap(string s)
+    {
+        //enviar parametros simples para a pagina destino
+        await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
+
+        //enviar objetos
+        //await Shell.Current.GoToAsync($"{nameof(DetailPage)}", new Dictionary<string, object>
+        //{
+        //    {nameof(DetailPage), s}
+        //});
+    }
+}
